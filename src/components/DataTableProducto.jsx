f67144b998440f967c800ab0066a618c224916a0
hamiltonh <hamiltonhernandez@gmail.com>
2021-09-30 08:35:53 -0500

    add estilos al data table productos

--- src/components/DataTableProducto.jsx
+++ src/components/DataTableProducto.jsx
@@ -6,31 +6,44 @@ const DataTableProducto = ({listaProductos}) => {
             {/* bootstrap  */}
             {/* Pendiente encabezado tabla */}
             {/* <h2></h2> */}
+                   
             <div class="table-responsive">
             {/*   table-striped  border-light*/}
-                <table class="table  table- table-hover  table-bordered caption-top">
+                <table class="table  table- table-hover  table-bordered caption-top table-listado">
                     <caption>Lista de productos</caption>
-                    <thead className='table-light'>
+                    <thead className='table-light text-center'>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Código</th>
                             <th scope="col">Descripción</th>
                             <th scope="col">Valor Unitarío</th>
                             <th scope="col">Estado</th>
-                            <th scope="col">Acción</th>
+                            <th scope="col">Acciónes</th>
                         </tr>
                     </thead>
                     <tbody>
                     {listaProductos.map((producto, index) => {
                     return (
-                        <tr>
+                        <tr className='text-center'>
                             <td>{index+1}</td>
                             <td>{producto.codigo}</td>
                             <td>{producto.descripcion}</td>
                             <td>{producto.valorUnit}</td>
                             <td>{producto.estado}</td>
-                            <td> 
-                                <button type="button" class="btn btn-secondary btn-sm">Editar</button>
+                            <td className="td_acciones"> 
+                                <button type="button" class="btn  btn-sm btn-outline-warning" title='Editar'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="orange" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                                    </svg>
+                                    {/* Editar */}
+                                </button>
+                                <button type="button" class="btn  btn-sm btn-sm btn-outline-danger" title='Eliminar'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="lightred" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                </svg>
+                                    {/* Eliminar */}
+                                </button>
                             </td>
                         </tr>
                          );
@@ -41,11 +54,11 @@ const DataTableProducto = ({listaProductos}) => {
 
             {/* paginator */}
             <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-end">
+                <ul class="pagination justify-content-center">
                     <li class="page-item disabled">
                     <a class="page-link" href="admin/productos">&laquo;</a>
                     </li>
-                    <li class="page-item"><a class="page-link" href="admin/productos">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="admin/productos">1</a></li>
                     <li class="page-item"><a class="page-link" href="admin/productos">2</a></li>
                     <li class="page-item"><a class="page-link" href="admin/productos">3</a></li>
                     <li class="page-item">
--- src/pages/admin/Productos.jsx
+++ src/pages/admin/Productos.jsx
@@ -7,38 +7,32 @@ import 'styles/productos.css';
 const productosBackend = [
   {
     codigo: '0001',
-    descripcion: 'Monitor Samsun 27"',
-    valorUnit: 768500,
+    descripcion: 'Monitor Samsung 27"',
+    valorUnit: 685000,
     estado: 'Disponible',
   },
   {
-    codigo: '0002',
-    descripcion: 'Monitor Samsun 27"',
-    valorUnit: 768500,
+    codigo: '0003',
+    descripcion: 'Disco SSD Kingston 256GB',
+    valorUnit: 168500,
+    estado: 'No Disponible',
+  },
+  {
+    codigo: '0004',
+    descripcion: 'Monitor LG 27"',
+    valorUnit: 568500,
     estado: 'Disponible',
   },
   {
-    codigo: '0002',
-    descripcion: 'Monitor Samsun 27"',
-    valorUnit: 768500,
-    estado: 'Disponible',
+    codigo: '0005',
+    descripcion: 'Mouse Gamer Genious"',
+    valorUnit: 212500,
+    estado: 'No Disponible',
   },
   {
-    codigo: '0002',
-    descripcion: 'Monitor Samsun 27"',
-    valorUnit: 768500,
-    estado: 'Disponible',
-  },
-  {
-    codigo: '0002',
-    descripcion: 'Monitor Samsun 27"',
-    valorUnit: 768500,
-    estado: 'Disponible',
-  },
-  {
-    codigo: '0002',
-    descripcion: 'Monitor Samsun 27"',
-    valorUnit: 768500,
+    codigo: '0006',
+    descripcion: 'Monitor Samsung 21"',
+    valorUnit: 568500,
     estado: 'Disponible',
   },
   
@@ -49,7 +43,7 @@ const Productos = () => {
   const [mostrarTabla, setMostrarTabla] = useState(true);
   const [productos, setProductos] = useState([]);
   const [textoBoton, setTextoBoton] = useState('Nuevo Producto');
-  const [colorBoton, setColorBoton] = useState('indigo');
+  const [colorBoton, setColorBoton] = useState('btn-success');
 
   useEffect(() => {
     //obtener lista  desde el backend
@@ -59,36 +53,37 @@ const Productos = () => {
   useEffect(() => {
     if (mostrarTabla) {
       setTextoBoton('Nuevo Producto');
-      setColorBoton('indigo');
+      setColorBoton('btn-success');
     } else {
       setTextoBoton('Mostrar Todos los productos');
-      setColorBoton('green');
+      setColorBoton('btn-info');
     }
   }, [mostrarTabla]);
   return (
-    <div className='flex h-full w-full flex-col items-center justify-start p-8 container container-productos'>
-      
+    // <div className='flex h-full w-full flex-col items-center justify-start p-8 container-productos'>
+    <div className='container-productos'>
       <div className='flex flex-col'>
-        <h2 className='text-3xl font-extrabold text-gray-900'>
-          Administrador de productos
-        </h2>
+        
+        <div className="container-title">
+          <h2 className='text-3xl font-extrabold text-gray-900'>
+            Administrador de productos
+          </h2>
+        </div>
         <br />
         <button
-          onClick={() => {
-            setMostrarTabla(!mostrarTabla);
-          }}
-          // className={`text-white bg-${colorBoton}-500 p-5 rounded-full m-6 w-28 self-end`}
-          className="btn btn-primary"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-          </svg>
-          {textoBoton}
-          
-        </button>
-        <br /><br />
+            onClick={() => {
+              setMostrarTabla(!mostrarTabla);
+            }}
+            // className={`text-white bg-${colorBoton}-500 p-5 rounded-full m-6 w-28 self-end`}
+            className={`btn ${colorBoton} btn-rounded`}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+            </svg>
+            <span>{textoBoton}</span>
+          </button>
       </div>
+
       {mostrarTabla ? (
         <TablaProductos listaProductos={productos} />
       ) : (
@@ -159,56 +154,74 @@ const FormularioCreacionProducto = ({ setMostrarTabla, listaProductos, setProduc
   };
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <h2 className='text-2xl font-extrabold text-gray-800'>Crear nuevo producto</h2>
-      <form ref={form} onSubmit={submitForm} className='flex flex-col'>
-        <label className='flex flex-col' htmlFor='nombre'>
-          Descripción del producto
-          <input
-            name='descripcion'
-            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
-            type='text'
-            placeholder='Ingrese descripcion'
-            required
-          />
-        </label>
-        <label className='flex flex-col' htmlFor='marca'>
-          Estado
-          <select
-            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
-            name='estado'
-            required
-            defaultValue={0}
-          >
-            <option disabled value={0}>
-              Seleccione una opción
-            </option>
-            <option>Disponible</option>
-            <option>No disponible</option>
-     
-          </select>
-        </label>
-        <label className='flex flex-col' htmlFor='modelo'>
-          Valor unitario
-          <input
-            name='valorUnit'
-            className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
-            type='number'
-            min={0}
-            max={9999999999999}
-            placeholder='Ingrese valor unitario'
-            required
-          />
-        </label>
 
-        <button
-          type='submit'
-          className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'
-        >
-          Guardar producto
-        </button>
-      </form>
-    </div>
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+
+    // <div className='flex flex-col items-center justify-center'>
+    //   <h2 className='text-2xl font-extrabold text-gray-800'>Crear nuevo producto</h2>
+    //   <form ref={form} onSubmit={submitForm} className='flex flex-col'>
+    //     <label className='flex flex-col' htmlFor='nombre'>
+    //       Descripción del producto
+    //       <input
+    //         name='descripcion'
+    //         className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+    //         type='text'
+    //         placeholder='Ingrese descripcion'
+    //         required
+    //       />
+    //     </label>
+    //     <label className='flex flex-col' htmlFor='marca'>
+    //       Estado
+    //       <select
+    //         className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+    //         name='estado'
+    //         required
+    //         defaultValue={0}
+    //       >
+    //         <option disabled value={0}>
+    //           Seleccione una opción
+    //         </option>
+    //         <option>Disponible</option>
+    //         <option>No disponible</option>
+     
+    //       </select>
+    //     </label>
+    //     <label className='flex flex-col' htmlFor='modelo'>
+    //       Valor unitario
+    //       <input
+    //         name='valorUnit'
+    //         className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+    //         type='number'
+    //         min={0}
+    //         max={9999999999999}
+    //         placeholder='Ingrese valor unitario'
+    //         required
+    //       />
+    //     </label>
+
+    //     <button
+    //       type='submit'
+    //       className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'
+    //     >
+    //       Guardar producto
+    //     </button>
+    //   </form>
+    // </div>
   );
 };
 
