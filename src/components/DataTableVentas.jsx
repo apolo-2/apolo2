ddfe7c8a486be4a22658c237cb8ee39276827490
hamartinezma <hamartinezma@gmail.com>
2021-10-03 12:41:44 -0500

    pull con los últimos cambios

--- src/components/DataTableVentas.jsx
+++ src/components/DataTableVentas.jsx
@@ -1,78 +1,101 @@
-import React from 'react'
+import React from "react";
 
-const DataTableVenta = ({listaVentas}) => {
-    return (
-        <div>
+const DataTableVenta = ({ listaVentas }) => {
+  return (
+    <div>
+      <div class="table-responsive">
+        <table class="table table-sm table-hover table-bordered caption-top table-listado">
+          <caption>Listado de ventas</caption>
+          <thead className="table-light text-center">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col"># Venta</th>
+              <th scope="col">Cliente</th>
+              <th scope="col">Vendedor</th>
+              <th scope="col">Total</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
 
-            <div class="table-responsive">
+          <tbody>
+            {listaVentas.map((venta, index) => {
+              return (
+                <tr className="text-center">
+                  <td>{index + 1}</td>
+                  {/* <td>{producto.codigo}</td> */}
+                  <td>000{index + 1}</td>
+                  <td>{venta.descripcion}</td>
+                  <td>{venta.valorUnit}</td>
+                  <td>{venta.estado}</td>
+                  <td className="td_acciones">
+                    <button
+                      type="button"
+                      class="btn  btn-sm btn-sm btn-outline-danger"
+                      title="Ver detalles"
+                    >
+                      <i class="fas fa-eye"></i>
+                    </button>
+                    <button
+                      type="button"
+                      class="btn  btn-sm btn-outline-warning"
+                      title="Editar"
+                    ></button>
+                    <button
+                      type="button"
+                      class="btn  btn-sm btn-sm btn-outline-danger"
+                      title="Eliminar"
+                      data-bs-toggle="modal"
+                      data-bs-target="#confirmDeleteModal"
+                    >
+                      <i class="far fa-trash-alt"></i>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
 
-                <table class="table  table-sm table-hover  table-bordered caption-top table-listado">
-
-                    <caption>Listado de ventas</caption>
-                    <thead className='table-light text-center'>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Código</th>
-                            <th scope="col">Descripción</th>
-                            <th scope="col">Valor Unitarío</th>
-                            <th scope="col">Estado</th>
-                            <th scope="col">Acciónes</th>
-                        </tr>
-                    </thead>
-
-
-
-                    <tbody>
-                        {listaVentas.map((producto, index) => {
-                        return (
-                            <tr className='text-center'>
-                                <td>{index+1}</td>
-                                {/* <td>{producto.codigo}</td> */}
-                                <td>000{index+1}</td>
-                                <td>{producto.descripcion}</td>
-                                <td>{producto.valorUnit}</td>
-                                <td>{producto.estado}</td>
-                                <td className="td_acciones">
-
-                                    <button type="button" class="btn  btn-sm btn-outline-warning" title='Editar'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="orange" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                                        </svg>
-                                    </button>
-
-                                    <button type="button" class="btn  btn-sm btn-sm btn-outline-danger" title='Eliminar'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="lightred" class="bi bi-trash" viewBox="0 0 16 16">
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                        </svg>
-                                    </button>
-
-                                </td>
-                            </tr>
-                            );
-                        })}
-                    </tbody>
-
-                </table>
+      {/* <!-- Modal --> */}
+      <div
+        class="modal fade"
+        id="confirmDeleteModal"
+        tabindex="-1"
+        aria-labelledby="confirmDeleteModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="confirmDeleteModalLabel">
+                Confirmación
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-
-
-            <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-center">
-                    <li class="page-item disabled">
-                    <a class="page-link" href="/admin/productos">&laquo;</a>
-                    </li>
-                    <li class="page-item active"><a class="page-link" href="/admin/productos">1</a></li>
-                    <li class="page-item"><a class="page-link" href="/admin/productos">2</a></li>
-                    <li class="page-item"><a class="page-link" href="/admin/productos">3</a></li>
-                    <li class="page-item">
-                    <a class="page-link" href="/admin/productos">&raquo;</a>
-                    </li>
-                </ul>
-            </nav>
-
+            <div class="modal-body">Desea eliminar el registro?</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Cancelar
+              </button>
+              <button type="button" class="btn btn-primary">
+                Confirmar
+              </button>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default DataTableVenta;
--- src/pages/admin/Ventas.jsx
+++ src/pages/admin/Ventas.jsx
@@ -1,49 +1,41 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import DataTableVentas from 'components/DataTableVentas';
-import 'styles/ventas.css';
+import React, { useEffect, useState, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import DataTableVentas from "components/DataTableVentas";
+import "styles/ventas.css";
 
 const VentasBackend = [
   {
-    codigo: '0001',
-    descripcion: 'Monitor Samsung 27"',
-    valorUnit: 685000,
-    estado: 'Disponible',
+    numVenta: "0001",
+    cliente: "Pablo Perez",
+    vendedor: 685000,
+    total: "Disponible",
   },
   {
-    codigo: '0002',
-    descripcion: 'Disco SSD Kingston 256GB',
-    valorUnit: 168500,
-    estado: 'No Disponible',
+    numVenta: "0002",
+    cliente: "Diana Duran",
+    vendedor: 168500,
+    total: "No Disponible",
   },
   {
-    codigo: '0003',
-    descripcion: 'Monitor LG 27"',
-    valorUnit: 568500,
-    estado: 'Disponible',
+    numVenta: "0003",
+    cliente: 'Monitor LG 27"',
+    vendedor: 568500,
+    total: "Disponible",
   },
   {
-    codigo: '0004',
-    descripcion: 'Mouse Gamer Genious"',
-    valorUnit: 212500,
-    estado: 'No Disponible',
+    numVenta: "0004",
+    cliente: 'Mouse Gamer Genious"',
+    vendedor: 212500,
+    total: "No Disponible",
   },
-  {
-    codigo: '0005',
-    descripcion: 'Monitor Samsung 21"',
-    valorUnit: 568500,
-    estado: 'Disponible',
-  },
-  
 ];
-
 
 const Ventas = () => {
   const [mostrarTabla, setMostrarTabla] = useState(true);
   const [Ventas, setVentas] = useState([]);
-  const [textoBoton, setTextoBoton] = useState('Nuevo Ventas');
-  const [colorBoton, setColorBoton] = useState('btn-secondary');
+  const [textoBoton, setTextoBoton] = useState("Nuevo Ventas");
+  const [colorBoton, setColorBoton] = useState("btn-secondary");
 
   useEffect(() => {
     //obtener lista  desde el backend
@@ -52,27 +44,38 @@ const Ventas = () => {
 
   useEffect(() => {
     if (mostrarTabla) {
-      setTextoBoton('Nuevo Ventas');
-      setColorBoton('btn-secondary');
+      setTextoBoton("Registrar venta");
+      setColorBoton("btn-secondary");
     } else {
-      setTextoBoton('Mostrar Todos los Ventas');
-      setColorBoton('btn-info');
+      setTextoBoton("Mostrar listado de ventas");
+      setColorBoton("btn-info");
     }
   }, [mostrarTabla]);
   return (
-
-    <div className='container-ventas'>
-      <div className=''>
+    <div className="container-ventas">
+      <div className="">
         <div className="container-title">
-          <h3 className=''>Gestión de Ventas</h3>
+          <h3 className="">Gestión de Ventas</h3>
         </div>
         <br />
-        <button onClick={() => {setMostrarTabla(!mostrarTabla);}} className={`btn ${colorBoton} btn-rounded`}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-            </svg>
-            <span>{textoBoton}</span>
-          </button>
+        <button
+          onClick={() => {
+            setMostrarTabla(!mostrarTabla);
+          }}
+          className={`btn ${colorBoton} btn-rounded`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            fill="currentColor"
+            class="bi bi-plus-circle-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+          </svg>
+          <span>{textoBoton}</span>
+        </button>
       </div>
 
       {mostrarTabla ? (
@@ -85,21 +88,23 @@ const Ventas = () => {
         />
       )}
 
-      <ToastContainer position='bottom-right' autoClose={5000} />
+      <ToastContainer position="bottom-right" autoClose={5000} />
     </div>
   );
 };
 
 const TablaVentas = ({ listaVentas }) => {
   useEffect(() => {
-    console.log('este es el listado  en el componente de tabla', listaVentas);
+    console.log("este es el listado  en el componente de tabla", listaVentas);
   }, [listaVentas]);
-  return (
-    <DataTableVentas  listaVentas={listaVentas}/>
-  );
+  return <DataTableVentas listaVentas={listaVentas} />;
 };
 
-const FormularioCreacionVentas = ({ setMostrarTabla, listaVentas, setVentas }) => {
+const FormularioCreacionVentas = ({
+  setMostrarTabla,
+  listaVentas,
+  setVentas,
+}) => {
   const form = useRef(null);
   const submitForm = (e) => {
     e.preventDefault();
@@ -110,9 +115,9 @@ const FormularioCreacionVentas = ({ setMostrarTabla, listaVentas, setVentas }) =
     });
     setMostrarTabla(true);
     setVentas([...listaVentas, nuevoVentas]);
-    console.log('nuevoVentas::',nuevoVentas);
+    console.log("nuevoVentas::", nuevoVentas);
     // identificar el caso de éxito y mostrar un toast de éxito
-    toast.success('Ventas agregado con éxito', {
+    toast.success("Ventas agregado con éxito", {
       // position: toast.POSITION.BOTTOM_RIGHT,
       // className: 'foo-bar'
     });
@@ -121,71 +126,88 @@ const FormularioCreacionVentas = ({ setMostrarTabla, listaVentas, setVentas }) =
   };
 
   return (
-        // form nuevo prod
-        <div className="container">
-          <br />
-          <h5 className=''>Formulario nuevo Ventas</h5>
-          
-          <form ref={form} onSubmit={submitForm} className=''>
+    // form nuevo prod
+    <div className="container">
+      <br />
+      <h5 className="">Formulario nuevo Ventas</h5>
 
-            <div class="mb-3 row">
-              <label for="descripcion" class="col-sm-2 col-form-label">Descripción: </label>
-              <div class="col-sm-9">
-                <input type="text"
-                  name='descripcion'
-                  className='form-control'
-                  placeholder='Ingrese descripción del Ventas'
-                  required />
-                {/* <div id="descHelp" class="form-text">Descripción del Ventas</div> */}
-              </div>
-            </div>
-
-            <div class="mb-3 row">
-              <label for="valorUnit" class="col-sm-2 col-form-label">Valor unitario: </label>
-              <div class="col-sm-9">
-                <input type="number"
-                  name='valorUnit'
-                  className='form-control'
-                  min={0}
-                  max={9999999999999}
-                  placeholder='Ingrese valor por unidad'
-                  required />
-                {/* <div id="descHelp" class="form-text">Descripción del Ventas</div> */}
-              </div>
-            </div>
-
-            <div class="mb-3 row">
-              <label for="estado" class="col-sm-2 col-form-label">Estado: </label>
-              <div class="col-sm-9">
-                  <select
-                    className='form-select'
-                    aria-label="Default select"
-                    name='estado'
-                    required
-                    defaultValue={0}
-                  >
-                    <option value="Disponible" selected>Disponible</option>
-                    <option value="No disponible">No disponible</option>
-                  </select>
-                {/* <div id="descHelp" class="form-text">Descripción del Ventas</div> */}
-              </div>
-            </div>
-
-            <div className="col-md-11 d-flex justify-content-end ">
-                <button
-                  type='submit'
-                  className='btn btn-primary btn-lg'
-                >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sd-card" viewBox="0 0 16 16">
-                  <path d="M6.25 3.5a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2zm2 0a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2zm2 0a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2zm2 0a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2z"/>
-                  <path fill-rule="evenodd" d="M5.914 0H12.5A1.5 1.5 0 0 1 14 1.5v13a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5V3.914c0-.398.158-.78.44-1.06L4.853.439A1.5 1.5 0 0 1 5.914 0zM13 1.5a.5.5 0 0 0-.5-.5H5.914a.5.5 0 0 0-.353.146L3.146 3.561A.5.5 0 0 0 3 3.914V14.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-13z"/>
-                </svg>
-                   Guardar
-                </button>
-            </div>
-          </form>   
+      <form ref={form} onSubmit={submitForm} className="">
+        <div class="mb-3 row">
+          <label for="cliente" class="col-sm-2 col-form-label">
+            Descripción:{" "}
+          </label>
+          <div class="col-sm-9">
+            <input
+              type="text"
+              name="cliente"
+              className="form-control"
+              placeholder="Ingrese descripción del Ventas"
+              required
+            />
+            {/* <div id="descHelp" class="form-text">Descripción del Ventas</div> */}
+          </div>
         </div>
-      
+
+        <div class="mb-3 row">
+          <label for="vendedor" class="col-sm-2 col-form-label">
+            Valor unitario:{" "}
+          </label>
+          <div class="col-sm-9">
+            <input
+              type="number"
+              name="vendedor"
+              className="form-control"
+              min={0}
+              max={9999999999999}
+              placeholder="Ingrese valor por unidad"
+              required
+            />
+            {/* <div id="descHelp" class="form-text">Descripción del Ventas</div> */}
+          </div>
+        </div>
+
+        <div class="mb-3 row">
+          <label for="total" class="col-sm-2 col-form-label">
+            total:{" "}
+          </label>
+          <div class="col-sm-9">
+            <select
+              className="form-select"
+              aria-label="Default select"
+              name="total"
+              required
+              defaultValue={0}
+            >
+              <option value="Disponible" selected>
+                Disponible
+              </option>
+              <option value="No disponible">No disponible</option>
+            </select>
+            {/* <div id="descHelp" class="form-text">Descripción del Ventas</div> */}
+          </div>
+        </div>
+
+        <div className="col-md-11 d-flex justify-content-end ">
+          <button type="submit" className="btn btn-primary btn-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-sd-card"
+              viewBox="0 0 16 16"
+            >
+              <path d="M6.25 3.5a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2zm2 0a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2zm2 0a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2zm2 0a.75.75 0 0 0-1.5 0v2a.75.75 0 0 0 1.5 0v-2z" />
+              <path
+                fill-rule="evenodd"
+                d="M5.914 0H12.5A1.5 1.5 0 0 1 14 1.5v13a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5V3.914c0-.398.158-.78.44-1.06L4.853.439A1.5 1.5 0 0 1 5.914 0zM13 1.5a.5.5 0 0 0-.5-.5H5.914a.5.5 0 0 0-.353.146L3.146 3.561A.5.5 0 0 0 3 3.914V14.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-13z"
+              />
+            </svg>
+            Guardar
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
