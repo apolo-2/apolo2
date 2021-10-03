42e396f76e525d2e3580659f3cbe1a0c87149d28
hamiltonh <hamiltonhernandez@gmail.com>
2021-10-02 23:37:51 -0500

    [A2-44]feat creo la notificacion para cuando se actualiza-crea un producto

--- src/components/DataTableProducto.jsx
+++ src/components/DataTableProducto.jsx
@@ -85,7 +85,9 @@ const DataTableProducto = ({listaProductos}) => {
                 <div class="modal-dialog">
                     <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="confirmDeleteModalLabel">Importante</h5>
+                        <h5 class="modal-title" id="confirmDeleteModalLabel">
+                            <i class="fas fa-exclamation-circle "></i> Importante
+                        </h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
--- src/components/context/darkMode.js
+++
@@ -1,6 +0,0 @@
-// ejemplo de uso de contexto
-// ... incluir contenido
-// Modificar tamben App.jsx
-//Modificar header
-// Se agregar buton darkmode como componente.
-// se modifica el index del admin
